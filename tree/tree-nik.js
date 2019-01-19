c76d795d4d479092356a40a2ed73a6e0e0c7d78c
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-01-19 19:56:06 +0530

    dfs tree

--- tree/tree-nik.js
+++ tree/tree-nik.js
@@ -15,10 +15,34 @@ class Tree{
     traverseDF(){
         (function traverse(currentNode){
 
-            console.log(currentNode+"=>");
+            console.log(currentNode.data+"=>");
             for(let i=0,length=currentNode.children.length;i<length;i++){
                 traverse(currentNode.children[i]);
             }
         })(this.root);
     }
 }
+
+var tree = new Tree('one');
+ 
+//dummy insertions
+//to check dfs
+tree.root.children.push(new Node('two'));
+tree.root.children[0].parent = tree;
+ 
+tree.root.children.push(new Node('three'));
+tree.root.children[1].parent = tree;
+ 
+tree.root.children.push(new Node('four'));
+tree.root.children[2].parent = tree;
+ 
+tree.root.children[0].children.push(new Node('five'));
+tree.root.children[0].children[0].parent = tree.root.children[0];
+ 
+tree.root.children[0].children.push(new Node('six'));
+tree.root.children[0].children[1].parent = tree.root.children[0];
+ 
+tree.root.children[2].children.push(new Node('seven'));
+tree.root.children[2].children[0].parent = tree.root.children[2];
+
+tree.traverseDF();
