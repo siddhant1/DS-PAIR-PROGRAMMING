c60080327b2bb9d9ed192e9bf897961dc7f1acca
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-01-22 23:03:06 +0530

    implemented BST in js

--- tree/binary_search_tree.js
+++ tree/binary_search_tree.js
@@ -13,32 +13,73 @@ class BinarySearchTree{
         this.root=new Node(value);
     }
 
-    add(value){
-        let node=new Node(value);
+    contains(value){
         let current=this.root;
 
+        if(value==this.root.data){
+            return true;
+        }
         while(current){
-            if(value<current.value){
-                if(!current.left){
-                    current.left=node;
-                    return;
-                }
-                else{
-                    current=current.left;
-                }
+            if(value==current.data){
+                return true;
             }
-
-            else{
-                if(!current.right){
-                    current.right=node;
-                    return;
-                }
                 else{
-                    current=current.right;
+                    if(value<current.data){
+                        if(!current.left){
+                            return false;
+                        }
+                        else{
+                            current=current.left;
+                        }
+                    }
+
+                    else if(value>current.data){
+                           if(!current.right){
+                               return false;
+                           }
+                           else{
+                               current=current.right;
+                           }
+
+                    }
                 }
+
             }
         }
 
+        add(value){
+            let node=new Node(value);
+            let current=this.root;
+    
+            while(current){
+                if(value<current.data){
+                    if(!current.left){
+                        current.left=node;
+                        return;
+                    }
+                    else{
+                        current=current.left;
+                    }
+                }
+    
+                else{
+                    if(!current.right){
+                        current.right=node;
+                        return;
+                    }
+                    else{
+                        current=current.right;
+                    }
+                }
+            }
+    
+        }
     }
 
-}
+    let bst=new BinarySearchTree(1);
+    bst.add(2);
+    bst.add(5);
+    console.log(bst.contains(2));
+
+
+
